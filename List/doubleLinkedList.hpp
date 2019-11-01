a570d5af3f579948c0f97266ef98bc7855ee74ef
ariolandi <stela_marinova@abv.bg>
2019-11-01 18:37:51 +0200

    Double linked list added

--- List/doubleLinkedList.hpp
+++ List/doubleLinkedList.hpp
@@ -1,27 +1,28 @@
-#ifndef _LIST_H_
-#define _LIST_H_
+#ifndef _DOUBLE_LIST_H_
+#define _DOUBLE_LIST_H_
 
 //Linked list class with iterator
 //fields for the first and the last element of the list
 //subclass for the iterator
 template <typename T>
-class List{
+class DoubleLinkedList{
     private:
         struct Node;
         Node *front, *back;
-        void copy_list(const List<T>&);
+        void copy_list(const DoubleLinkedList<T>&);
         void delete_list();
     public:
         class iterator;
 
-        List();
-        List(const List<T>&);
-        List<T>& operator=(const List<T>&);
-        ~List();
+        DoubleLinkedList();
+        DoubleLinkedList(const DoubleLinkedList<T>&);
+        DoubleLinkedList<T>& operator=(const DoubleLinkedList<T>&);
+        ~DoubleLinkedList();
 
         bool empty()const;
         void push_back(const T);
         void insert_after(iterator, const T);
+        void insert_before(iterator, const T);
         void erase(iterator);
 
         iterator begin()const;
@@ -31,17 +32,17 @@ class List{
 //the structure of a list's element
 //each element has a value field and a pointer (to the next element or NULL by default)
 template <typename T>
-struct List<T>::Node{
+struct DoubleLinkedList<T>::Node{
     T value;
-    Node* next;
+    Node* next, *prev;
     Node(const T& value_): value(value_){
-        next = nullptr;
+        next = prev = nullptr;
     }
 };
 
 //copy function
 template <typename T>
-void List<T>::copy_list(const List<T>& other){
+void DoubleLinkedList<T>::copy_list(const DoubleLinkedList<T>& other){
     front = nullptr;
     Node *ptr = other.front;
     while(ptr != nullptr){
@@ -52,7 +53,7 @@ void List<T>::copy_list(const List<T>& other){
 
 //delete function
 template <typename T>
-void List<T>::delete_list(){
+void DoubleLinkedList<T>::delete_list(){
     while(front != nullptr){
         Node *ptr = front;
         front = front->next;
@@ -62,21 +63,21 @@ void List<T>::delete_list(){
 }
 
 //constructor
-//in the beginning every list is empty
+//in the beginning every DoubleLinkedList is empty
 template <typename T>
-List<T>::List(){
+DoubleLinkedList<T>::DoubleLinkedList(){
     front = back = nullptr;
 }
 
 //copy constructor
 template <typename T>
-List<T>::List(const List<T>& other){
+DoubleLinkedList<T>::DoubleLinkedList(const DoubleLinkedList<T>& other){
     copy_list(other);
 }
 
 //assignment operator
 template <typename T>
-List<T>& List<T>::operator=(const List<T>& other){
+DoubleLinkedList<T>& DoubleLinkedList<T>::operator=(const DoubleLinkedList<T>& other){
     if(this != &other){
         delete_list();
         copy_list(other);
@@ -87,33 +88,35 @@ List<T>& List<T>::operator=(const List<T>& other){
 
 //destructor
 template <typename T>
-List<T>::~List(){
+DoubleLinkedList<T>::~DoubleLinkedList(){
     delete_list();
 }
 
 
 //a list is empty if the first element pointer is NULL
 template <typename T>
-bool List<T>::empty()const{
+bool DoubleLinkedList<T>::empty()const{
     return (front == nullptr);
 }
 
 //add function
 //adds a new element to the end of the list
 template <typename T>
-void List<T>::push_back(const T value_){
+void DoubleLinkedList<T>::push_back(const T value_){
     Node *node = new Node(value_);
     if(empty())
         front = node;
-    else
+    else{
         back->next = node;
+        node->prev = back;
+    }
     back = node;
 }
 
 //add function
 //adds a new element after a certain position in the list
 template <typename T>
-void List<T>::insert_after(iterator position, const T value_){
+void DoubleLinkedList<T>::insert_after(iterator position, const T value_){
     if(position == end() || empty()){
         push_back(value_);
         return;
@@ -121,29 +124,43 @@ void List<T>::insert_after(iterator position, const T value_){
     Node *node = new Node(value_);
     node->next = position.ptr->next;
     position.ptr->next = node;
+    (node->next)->prev = node;
+    node->prev = position.ptr;
+}
+
+//add function
+//adds a new element before a certain position in the list
+template <typename T>
+void DoubleLinkedList<T>::insert_before(iterator position, const T value_){
+    if(empty()){
+        push_back(value_);
+        return;
+    }
+    Node *node = new Node(value_);
+    node->next = position.ptr;
+    if(position != begin()){
+        (position.ptr->prev)->next = node;
+        node->prev = position.ptr->prev;
+    }
+    position.ptr->prev = node;
 }
 
 //erase function
 //removes the element of the list on certain position
 //Warning: the iterator is being moved to the next element
 template<typename T>
-void List<T>::erase(iterator position){
+void DoubleLinkedList<T>::erase(iterator position){
     if(empty())
         return;
     if(position == end()){
-        Node* ptr = front;
-        while(ptr->next != back){
-            ptr = ptr->next;
-        }
+        Node* ptr = back->prev;
         delete back;
         back = ptr;
     }
     else{
-        Node* ptr = front;
-        while(ptr->next != position.ptr){
-            ptr = ptr->next;
-        }
-        ptr->next = position.ptr->next;
+        Node* ptr = position.ptr->next;
+        ptr->prev = position.ptr->prev;
+        (position.ptr->prev)->next = ptr;
         delete position.ptr;
         position.ptr = ptr->next;
     }
@@ -151,14 +168,14 @@ void List<T>::erase(iterator position){
 
 //returns an iterator to the first element of the list
 template <typename T>
-typename List<T>::iterator List<T>::begin()const{
+typename DoubleLinkedList<T>::iterator DoubleLinkedList<T>::begin()const{
     return iterator(front);
 }
 
 //returns an iterator to the end of the list
 //Warning: not the last element
 template <typename T>
-typename List<T>::iterator List<T>::end()const{
+typename DoubleLinkedList<T>::iterator DoubleLinkedList<T>::end()const{
     if(back != nullptr)
         return iterator(back->next);
     return iterator(nullptr);
@@ -168,7 +185,7 @@ typename List<T>::iterator List<T>::end()const{
 //class Iterator
 //field: pointer to a node of a list
 template <typename T>
-class List<T>::iterator{
+class DoubleLinkedList<T>::iterator{
     private:
         Node *ptr;
     public:
@@ -178,67 +195,88 @@ class List<T>::iterator{
         bool operator!=(const iterator&)const;
         bool operator==(const iterator&)const;
         iterator& operator++();
+        iterator& operator--();
         iterator operator+(const unsigned int);
+        iterator operator-(const unsigned int);
         T& operator*();
 
-        friend void List<T>::insert_after(iterator, const T);
-        friend void List<T>::erase(iterator);
+        friend void DoubleLinkedList<T>::insert_after(iterator, const T);
+        friend void DoubleLinkedList<T>::insert_before(iterator, const T);
+        friend void DoubleLinkedList<T>::erase(iterator);
 };
 
 //constructor
 template <typename T>
-List<T>::iterator::iterator(){
+DoubleLinkedList<T>::iterator::iterator(){
     ptr = nullptr;
 }
 
 //copy constructor
 template <typename T>
-List<T>::iterator::iterator(Node* other){
+DoubleLinkedList<T>::iterator::iterator(Node* other){
     ptr = other;
 }
 
 //assignment operator
 template <typename T>
-typename List<T>::iterator& List<T>::iterator::operator=(const iterator& other){
+typename DoubleLinkedList<T>::iterator& DoubleLinkedList<T>::iterator::operator=(const iterator& other){
     ptr = other.ptr;
 }
 
 //operator not-equal
 template <typename T>
-bool List<T>::iterator::operator!=(const iterator& other)const{
+bool DoubleLinkedList<T>::iterator::operator!=(const iterator& other)const{
     return ptr != other.ptr;
 }
 
 //operator equal
 template <typename T>
-bool List<T>::iterator::operator==(const iterator& other)const{
+bool DoubleLinkedList<T>::iterator::operator==(const iterator& other)const{
     return ptr == other.ptr;
 }
 
 //moving operator
 //moves the pointer to the next node
 template <typename T>
-typename List<T>::iterator& List<T>::iterator::operator++(){
+typename DoubleLinkedList<T>::iterator& DoubleLinkedList<T>::iterator::operator++(){
     ptr = ptr->next;
 }
+
+//moving operator
+//moves the pointer to the previous node
+template <typename T>
+typename DoubleLinkedList<T>::iterator& DoubleLinkedList<T>::iterator::operator--(){
+    ptr = ptr->prev;
+}
+
 //moving operator
 //moves the pointer n nodes forward
 template <typename T>
-typename List<T>::iterator List<T>::iterator::operator+(const unsigned int n){
+typename DoubleLinkedList<T>::iterator DoubleLinkedList<T>::iterator::operator+(const unsigned int n){
     Node* p = ptr;
     for(int i=0; i<n && p!=nullptr; i++)
         p = p->next;
     return iterator(p);
 }
 
+//moving operator
+//moves the pointer n nodes backward
+template <typename T>
+typename DoubleLinkedList<T>::iterator DoubleLinkedList<T>::iterator::operator-(const unsigned int n){
+    Node* p = ptr;
+    for(int i=0; i<n && p!=nullptr; i++)
+        p = p->prev;
+    return iterator(p);
+}
+
 //dereferencing operator
 //returns the value of the pointed node
 template <typename T>
-T& List<T>::iterator::operator*(){
+T& DoubleLinkedList<T>::iterator::operator*(){
     return ptr->value;
 }
 
 
 
 
-#endif // _LIST_H_
+#endif // _DOUBLE_LIST_H_
